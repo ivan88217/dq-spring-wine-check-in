bffd4390f3c69c45884f00abf451d40012055a3f
ivan88217 <ivan88217@gmail.com>
2024-02-24 22:58:13 +0800

    第一版展示頁面

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
+"use client";
+
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import type { MouseEventHandler, FormEventHandler } from "react";
+import { useEffect, useState } from "react";
+import { AlertError } from "@/components/alert-error";
+import { getCookies, setCookie } from "@/lib/cookie-parser";
 
 export default function Home() {
+  const cookieName = "isChecked";
+  const [code, setCode] = useState("");
+  const [checkInDisabled, setCheckInDisabled] = useState(true);
+  const [btnText, setBtnText] = useState("簽到");
+  const [error, setError] = useState<{
+    title: string;
+    text?: string;
+  }>({
+    title: "Error",
+    text: "description",
+  });
+  const [errorShown, setErrorShown] = useState(false);
+  const [currentTimeoutHandler, setCurrentTimeoutHandler] =
+    useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const cookies = getCookies();
+    if (!cookies.isChecked) {
+      setCheckInDisabled(false);
+      return;
+    }
+    setCode(cookies.isChecked);
+  });
+
+  useEffect(() => {
+    const text = checkInDisabled ? `${code} 簽到成功` : "簽到";
+
+    setBtnText(text);
+  }, [checkInDisabled, code]);
+
+  const errorShow = (title: string, text?: string) => {
+    setError({
+      title,
+      text,
+    });
+    setErrorShown(true);
+    if (currentTimeoutHandler) clearTimeout(currentTimeoutHandler);
+    const timeout = setTimeout(() => setErrorShown(false), 5000);
+    setCurrentTimeoutHandler(timeout);
+    return;
+  };
+
+  const handleInput: FormEventHandler<HTMLInputElement> = (e) => {
+    setCode(e.currentTarget.value);
+  };
+
+  const handleSubmit: MouseEventHandler<HTMLButtonElement> = (e) => {
+    if (!code) {
+      errorShow("員工編號錯誤", "請輸入員工編號");
+      return;
+    }
+    setCookie("isChecked", code, 60 * 60);
+    setCheckInDisabled(true);
+    alert(`${code} 簽到成功`);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center justify-between p-10">
+      <Image
+        src="/icon.jpg"
+        alt="敦謙2024年春酒-自助簽到系統"
+        className="rounded-md object-cover"
+        width={150}
+        height={150}
+      />
+      <h1 className="text-2xl font-bold text-white mt-2">敦謙2024年春酒</h1>
+      <h1 className="text-3md font-bold text-white mt-2">自助簽到系統</h1>
+      <div className="m-5 flex flex-col w-4/5 text-center justify-between p-1">
+        <div>
+          <AlertError
+            className="p-2 mb-3"
+            title={error.title}
+            text={error.text}
+            hidden={!errorShown}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <Label htmlFor="code">員工編號</Label>
+        <Input
+          id="code"
+          className="m-1 text-center border-green-800 bg-gray-900"
+          placeholder="請輸入員工編號"
+          onInput={handleInput}
         />
+        <Button
+          className="m-1 w-[100%]"
+          variant="secondary"
+          onClick={handleSubmit}
+          disabled={checkInDisabled}
+        >
+          {btnText}
+        </Button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
