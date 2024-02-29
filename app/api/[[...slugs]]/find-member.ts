9a7ab98d3258ef2ab929820c14193f1767ba8eed
ivan88217 <ivan88217@gmail.com>
2024-02-29 21:17:16 +0800

    更新搜尋員工功能以支援模糊搜尋

--- app/api/[[...slugs]]/find-member.ts
+++ app/api/[[...slugs]]/find-member.ts
@@ -4,11 +4,16 @@ import { Elysia, t } from "elysia";
 export const findMemberController = new Elysia().get(
   "/find-member",
   async ({ query }) => {
+    const name = query.name;
+    if (!name) {
+      return [];
+    }
+
     // 以名稱搜尋員工(模糊搜尋)
     const members = await prisma.member.findMany({
       where: {
         name: {
-          contains: query.name,
+          contains: name,
         },
       },
       select: {
