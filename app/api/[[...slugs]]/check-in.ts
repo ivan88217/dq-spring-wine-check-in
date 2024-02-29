f787593c8027f486b0e0f753f176f556bc53a604
ivan <ivan.shi@dunqian.com>
2024-02-29 17:16:24 +0800

    改為使用 prisma

--- app/api/[[...slugs]]/check-in.ts
+++ app/api/[[...slugs]]/check-in.ts
@@ -1,9 +1,40 @@
 import { Elysia, t } from "elysia";
+import prisma from "@/lib/prisma";
 
 export const checkInController = new Elysia().post(
   "/check-in",
   async ({ body }) => {
-    // Do something with the body
+    const user = await prisma.member.findFirst({
+      where: {
+        code: body.code,
+      },
+    });
+
+    if (!user) {
+      throw new Error("找不到此員工編號");
+    }
+
+    const isChecked = await prisma.checkIn.findFirst({
+      where: {
+        memberId: user.id,
+      },
+    });
+
+    if (isChecked) {
+      throw new Error(`${user.code} 已經簽到過了`);
+    }
+
+    const checkIn = await prisma.checkIn.create({
+      data: {
+        member: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    return true;
   },
   {
     body: t.Object({
--- app/api/[[...slugs]]/is-checked.ts
+++ app/api/[[...slugs]]/is-checked.ts
@@ -1,15 +1,34 @@
-import { callScript } from "@/lib/app-script";
+import prisma from "@/lib/prisma";
 import { Elysia, t } from "elysia";
 
 export const isCheckedController = new Elysia().get(
   "/is-checked",
   async ({ query }) => {
-    // Do something with the query
+    const user = await prisma.member.findFirst({
+      where: {
+        code: query.code,
+      },
+    });
+
+    if (!user) {
+      throw new Error("找不到此員工編號");
+    }
+
+    const isChecked = await prisma.checkIn.findFirst({
+      where: {
+        memberId: user.id,
+      },
+    });
+
+    return !!isChecked;
   },
   {
     query: t.Object({
       code: t.String(),
     }),
     response: t.Boolean(),
+    error({ error }) {
+      return error.message;
+    },
   }
 );
