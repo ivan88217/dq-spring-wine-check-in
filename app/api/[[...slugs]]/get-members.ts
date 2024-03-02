2363fc56f90b3526f028a32ed2ab48f6d9556fb3
ivan88217 <ivan88217@gmail.com>
2024-03-02 12:34:15 +0800

    新增成員取得控制器

---
+++ app/api/[[...slugs]]/get-members.ts
@@ -0,0 +1,49 @@
+import { Elysia, t } from "elysia";
+import prisma from "@/lib/prisma";
+
+export const getMembersController = new Elysia().get(
+  "/get-members",
+  async ({ query }) => {
+    const members = await prisma.member.findMany({
+      select: {
+        code: true,
+        name: true,
+        departmentName: true,
+      },
+      where: {
+        isAttend: query.isAttend,
+        isStay: query.isStay,
+        isBoss: query.isBoss,
+        isSubCompany: query.isSubCompany,
+        checkIn: query.isCheckIn
+          ? {
+              isNot: null,
+            }
+          : {
+              is: null,
+            },
+      },
+    });
+
+    return members;
+  },
+  {
+    query: t.Object({
+      isAttend: t.Optional(t.BooleanString()),
+      isStay: t.Optional(t.BooleanString()),
+      isBoss: t.Optional(t.BooleanString()),
+      isSubCompany: t.Optional(t.BooleanString()),
+      isCheckIn: t.Optional(t.BooleanString()),
+    }),
+    response: t.Array(
+      t.Object({
+        code: t.String(),
+        name: t.String(),
+        departmentName: t.Nullable(t.String()),
+      })
+    ),
+    error({ error }) {
+      return error.message;
+    },
+  }
+);
--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -2,10 +2,12 @@ import { Elysia, t } from "elysia";
 import { isCheckedController } from "./is-checked";
 import { checkInController } from "./check-in";
 import { findMemberController } from "./find-member";
+import { getMembersController } from "./get-members";
 
 const app = new Elysia({ prefix: "/api" })
   .use(isCheckedController)
   .use(checkInController)
+  .use(getMembersController)
   .use(findMemberController);
 
 export const GET = app.handle;
