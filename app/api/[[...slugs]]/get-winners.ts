dda381407fca1b9495de1edf22c285cfdb743d4b
ivan88217 <ivan88217@gmail.com>
2024-03-02 13:25:28 +0800

    新增獲獎者控制器

---
+++ app/api/[[...slugs]]/get-winners.ts
@@ -0,0 +1,45 @@
+import { Elysia, t } from "elysia";
+import prisma from "@/lib/prisma";
+
+export const getWinnersController = new Elysia().get(
+  "/get-winners",
+  async () => {
+    const winners = await prisma.memberPrize.findMany({
+      include: {
+        member: {
+          select: {
+            code: true,
+            name: true,
+            departmentName: true,
+          },
+        },
+        prize: {
+          select: {
+            name: true,
+          },
+        }
+      },
+    });
+
+    // 名稱第二字需要隱藏，例如：王小明 -> 王*明
+    return winners.map((winner) => ({
+      code: winner.member.code,
+      name: winner.member.name.replace(/(?<=^.{1})./g, "*"),
+      departmentName: winner.member.departmentName,
+      prize: winner.prize.name,
+    }));
+  },
+  {
+    response: t.Array(
+      t.Object({
+        code: t.String(),
+        name: t.String(),
+        departmentName: t.Nullable(t.String()),
+        prize: t.String(),
+      })
+    ),
+    error({ error }) {
+      return error.message;
+    },
+  }
+);
--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -4,12 +4,14 @@ import { checkInController } from "./check-in";
 import { findMemberController } from "./find-member";
 import { getMembersController } from "./get-members";
 import { winPrizeController } from "./win-prize";
+import { getWinnersController } from "./get-winners";
 
 const app = new Elysia({ prefix: "/api" })
   .use(winPrizeController)
   .use(isCheckedController)
   .use(checkInController)
   .use(getMembersController)
+  .use(getWinnersController)
   .use(findMemberController);
 
 export const GET = app.handle;
