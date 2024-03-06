e63aafeb868772c4cc9622d1cddc29dc59b2837b
ivan <ivan.shi@dunqian.com>
2024-03-06 14:28:49 +0800

    新增敦謙春酒得獎名單的metadata

--- app/winners/page.tsx
+++ app/winners/page.tsx
@@ -2,6 +2,12 @@ import { DataTable } from "./data-table";
 import { Winner, columns } from "./columns";
 import prisma from "@/lib/prisma";
 import { unstable_noStore as noStore } from 'next/cache';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "敦謙春酒得獎名單",
+  description: "敦謙集團 2024年 春酒得獎名單",
+};
 
 async function getData(): Promise<Winner[]> {
   noStore();
