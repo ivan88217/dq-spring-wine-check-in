1009809218cbc23e5cbafbd52c8a711b266b36ec
ivan <ivan.shi@dunqian.com>
2024-03-12 17:53:10 +0800

    更新菜單頁面，添加投票頁面和隊伍管理功能

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -13,13 +13,18 @@ export default function Menu() {
           </Link>
         </Button>
         <Button variant={"secondary"} className="w-full">
-          <Link href="/admin" className="w-full">
-            簽到頁面-管理者模式
+          <Link href="/votes" className="w-full">
+            投票頁面
           </Link>
         </Button>
         <Button variant={"secondary"} className="w-full">
-          <Link href="/votes" className="w-full">
-            投票頁面
+          <Link href="/teams" className="w-full">
+            隊伍管理
+          </Link>
+        </Button>
+        <Button variant={"secondary"} className="w-full">
+          <Link href="/admin" className="w-full">
+            簽到頁面-管理者模式
           </Link>
         </Button>
         <Button variant={"secondary"} className="w-full">
@@ -30,11 +35,6 @@ export default function Menu() {
         <Button variant={"secondary"} className="w-full">
           <Link href="/winners" className="w-full">
             抽籤結果頁面
-          </Link>
-        </Button>
-        <Button variant={"secondary"} className="w-full">
-          <Link href="/teams" className="w-full">
-            隊伍管理
           </Link>
         </Button>
       </div>
